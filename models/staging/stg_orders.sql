ad9b71e5aa7a4eddd32a8037cb697cce6094bf4f
speak2kv@gmail.com <speak2kv@gmail.com>
2023-04-26 18:45:10 +0000

    added seed and added it to stg orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,9 +18,13 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice', 'ordercostprice') }} as markup
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+-- from delivery teams
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_teams') }} as d
+on o.shipmode = d.shipmode
